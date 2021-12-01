ee41e4a143871960da5d6ddb2290fa06cb3a5521
Sarah Lim <sarah@sarahlim.com>
2021-11-30 21:51:08 -0800

    Extract argument input-output pairs
    Given an input program, we can now compute the set of input-output pairs
    `(width, isHoisted)`. These examples will be used to synthesize guards.

--- src/Mime/Transform/LetHoist.hs
+++ src/Mime/Transform/LetHoist.hs
@@ -2,25 +2,64 @@
 
 module Mime.Transform.LetHoist
     ( getBindings
+    , LetState (..)
+    , Argument (..)
     ) where
 
 import Data.Map (Map)
 import qualified Data.Map as M
-import Mime.Document (IntoDoc (width))
+import qualified Mime.Document as Doc (IntoDoc (width))
 import Mime.Grammar (Expr (..), Lit (..), Name (..))
+
+data Argument = Argument 
+    { isBound :: Bool -- | Is the argument bound or inline?
+    , width :: Int    -- | Width of the formatted argument. If bound, use the width of the bound value.
+    } deriving (Show, Eq)
+
+data LetState = LetState 
+    { bound :: Map Name Int -- | Accumulated let-bindings and bound value widths.
+    , args :: [Argument] -- | Accumulated arguments.
+    }
+
+emptyState :: LetState
+emptyState = LetState M.empty []
+
+-- | Adds a new let binding to the state.
+addBinding :: Name -> Expr -> LetState -> LetState
+addBinding x v (LetState bs as) = LetState bs' as
+    where bs' = M.insert x (Doc.width v) bs
+
+-- | Adds a new argument used in an application.
+addArgument :: LetState -> Expr -> LetState
+addArgument (LetState bs as) e = LetState bs (arg:as)
+    where arg = mkArgument bs e
+
+mkArgument :: Map Name Int -> Expr -> Argument
+mkArgument bs v@(Var x) = case M.lookup x bs of
+   Just w -> Argument True w
+   -- If it's not bound, compute the width of the identifier.
+   Nothing -> Argument False (Doc.width v)
+mkArgument _ v = Argument False (Doc.width v)
 
 -- | Let-hoisting transformation.
 -- | Finds all of the arguments used in function applications,
 -- | and computes their formatted widths.
-getBindings :: Expr -> Map Name Int
-getBindings = go M.empty
-    where go :: Map Name Int -> Expr -> Map Name Int
+getBindings :: Expr -> LetState
+getBindings = go emptyState
+    where go :: LetState -> Expr -> LetState
           go m = \case
-            Let x v b -> go m'' b
+            Par e        -> go m e
+            Lam _ body   -> go m body
+
+            Let x v body -> go m'' body
                 where m'  = go m v -- Recur into v, get bindings.
-                      m'' = M.insert x (width v) m'  -- Add x = v itself.
-            Lam _ b   -> go m b
-            App f vs  -> go m' f
-                where m' = foldr (flip go) m vs -- Traverse the arguments first.
-            Par e     -> go m e
+                      m'' = addBinding x v m'  -- Add x = v itself.
+
+            App f vs     -> go (foldr traverseArg m vs) f
+                where traverseArg v acc = do
+                        let acc' = addArgument acc v
+                        go acc' v -- Recur into argument.
+
             _         -> m
+
+
--- test/Mime/Transform/LetHoistSpec.hs
+++ test/Mime/Transform/LetHoistSpec.hs
@@ -8,7 +8,7 @@ import qualified Data.Text as T
 import Test.Hspec
 import Mime.Document (IntoDoc (..))
 import Mime.Grammar
-import Mime.Transform.LetHoist (getBindings)
+import Mime.Transform.LetHoist (getBindings, LetState (..), Argument (..))
 
 spec :: Spec 
 spec = describe "getBindings" $ do
@@ -26,9 +26,18 @@ spec = describe "getBindings" $ do
                            App (Var (Name "id")) [Var (Name "id2")])
                     , Lit (LNum 433) ]
 
-        getBindings e `shouldBe` M.fromList 
+        let result = getBindings e
+
+        bound result `shouldBe` M.fromList 
             [ (Name "id", T.length "\\x -> x")
             , (Name "id2", T.length "\\y -> y 162")
+            ]
+
+        args result `shouldBe` 
+            [ Argument True (T.length "\\y -> y 162") -- id2
+            , Argument False 3 -- 162
+            , Argument False (T.length "(let id2 = \\y -> y 162 in") -- (let id2 ...)
+            , Argument False 3 -- 433
             ]
 
     it "handles nested let without shadowing" $ do 
@@ -42,11 +51,15 @@ spec = describe "getBindings" $ do
                                    (Lit (LNum 1)))))
                     (Lit (LNum 22))
 
-        getBindings e `shouldBe` M.fromList 
+        let result = getBindings e
+
+        bound result `shouldBe` M.fromList 
             [ (Name "outer", 21)
             , (Name "inner", T.length "111")
             , (Name "inner2", T.length "2222")
             ]
+
+        args result `shouldBe` []
 
     it "handles nested let with shadowing" $ do 
         -- let outer = (let inner = 111 in
@@ -59,10 +72,14 @@ spec = describe "getBindings" $ do
                                    (Lit (LNum 1)))))
                     (Lit (LNum 22))
 
-        getBindings e `shouldBe` M.fromList 
+        let result = getBindings e
+
+        bound result `shouldBe` M.fromList 
             [ (Name "outer", 20)
             , (Name "inner", T.length "2222")
             ]
+
+        args result `shouldBe` []
 
     it "handles let bindings in applications" $ do 
         -- (\x -> let foo = 1 in
@@ -83,29 +100,50 @@ spec = describe "getBindings" $ do
                                  Var (Name "baz")
                     ]
 
-        getBindings e `shouldBe` M.fromList
+        let result = getBindings e
+
+        bound result `shouldBe` M.fromList
             [ (Name "foo", 1)
             , (Name "bar", 2)
             , (Name "baz", 3)
+            ]
+
+        args result `shouldBe`
+            [ Argument False 1 -- x
+            , Argument False (T.length "(let bar = 22 in")
+            , Argument False 1 -- 1
+            , Argument False (T.length "(let baz = 333 in")
             ]
         
 
     it "traverses into lambdas" $ do 
         -- let outer = \foo -> let inner = 111 in
         --                     let inner2 = 2222 in 
-        --                         1 in
-        --     33
+        --                         inner inner2 in
+        --     f outer 
+        --       33
         let e = Let (Name "outer")
                     (Lam [Name "foo"] $
                          Let (Name "inner") (Lit (LNum 111)) $
                              Let (Name "inner2") (Lit (LNum 2222)) $
-                                 Lit (LNum 1))
-                    (Lit (LNum 33))
+                                 App (Var (Name "inner"))
+                                     [Var (Name "inner2")])
+                    (App (Var (Name "f"))
+                         [Var (Name "outer")
+                         ,Lit (LNum 33)])
                     
-        getBindings e `shouldBe` M.fromList 
+        let result = getBindings e
+
+        bound result `shouldBe` M.fromList
             [ (Name "outer", 28)
             , (Name "inner", T.length "111")
             , (Name "inner2", T.length "2222")
+            ]
+
+        args result `shouldBe` 
+            [ Argument True (T.length "\\foo -> let inner2 = 2222 in")
+            , Argument False 2 -- 33
+            , Argument True (T.length "2222") -- inner2
             ]
 
     it "does the right thing at a given subtree?" $ do 

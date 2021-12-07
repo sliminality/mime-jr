253ab97a0c3f4d4097ae4d9103c4e2ea8dd022b8
Sarah Lim <sarah@sarahlim.com>
2021-12-06 18:24:02 -0800

    Show guards

--- src/Mime/Guard.hs
+++ src/Mime/Guard.hs
@@ -10,7 +10,6 @@ import Mime.Document (IntoDoc (width))
 import Mime.Grammar
 
 data N = NWidth | NConst Int
-    deriving (Show)
 
 data B = BTrue
        | BFalse
@@ -19,7 +18,6 @@ data B = BTrue
        | BAnd B B
        | BOr  B B
        | BNot B
-       deriving (Show)
 
 class Denote a b | a -> b where
     denote :: a -> Expr -> b
@@ -36,3 +34,16 @@ instance Denote B Bool where
     denote (BAnd b1 b2) e = denote b1 e && denote b2 e
     denote (BOr  b1 b2) e = denote b1 e || denote b2 e
     denote (BNot b)     e = not $ denote b e
+
+instance Show N where
+    show NWidth = "width"
+    show (NConst x) = show x
+
+instance Show B where
+    show BTrue = "⊤"
+    show BFalse = "⊥"
+    show (n1 `BGt` n2) = show n1 ++ " > " ++ show n2
+    show (n1 `BEq` n2) = show n1 ++ " = " ++ show n2
+    show (b1 `BAnd` b2) = show b1 ++ " && " ++ show b2
+    show (b1 `BOr` b2) = show b1 ++ " || " ++ show b2
+    show (BNot b) = "!" ++ show b

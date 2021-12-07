417aaba19c5f43a2d3d7721c860e2f3e81760487
Sarah Lim <sarah@sarahlim.com>
2021-12-06 18:24:13 -0800

    Demo

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,30 +4,41 @@ module Lib
     ( someFunc
     ) where
 
+import Data.Bifunctor (second)
+import Mime.Document (IntoDoc (width))
 import Mime.Grammar
 import Mime.Transform.LetHoist (getExamples)
 import Mime.Synthesis (synth)
 
 sample :: Expr
 {- 
+  let myList = "abcdefghijklmnopqrstuvwxyz" in
   let add = \x, y -> plus x y in
-      foldr add 0 "hello"
+      foldr add 0 myList
  -}
-sample = Let (Name "add")
-             (Lam [Name "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", Name "y"] $
+
+sample = Let (Name "myList")
+             (Lit (LStr "abcdefghijklmnopqrstuvwxyz")) $
+         Let (Name "add")
+             (Lam [Name "x", Name "y"] $
                   App (Var (Name "plus")) 
                       [Var (Name "x"), Var (Name "y")]) $
              App (Var (Name "foldr")) 
                  [ Var (Name "add")
                  , Lit (LNum 0)
-                 , Lit (LStr "hello")
+                 , Var (Name "myList")
                  ]
 
 someFunc :: IO ()
 someFunc = do
-    let xs = getExamples sample
-    print xs
+    putStrLn "\n******** Synthesizing bounds for program:"
+    print sample
 
+    putStrLn "\n******** Extracting examples:"
+    let xs = getExamples sample
+    print $ second width <$> xs
+
+    putStrLn "\n******** Synthesized bound:"
     case synth xs 100 of
       Just x -> print x
       _ -> print "No solution found"
